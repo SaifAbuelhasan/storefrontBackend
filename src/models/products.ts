a0d611e162b4df6e4042e72620864429fddeddb1
Saif Abuelhasan <saif@DESKTOP-BD63ERL.localdomain>
2022-07-15 03:50:21 +0300

    products model

--- src/models/products.ts
+++ src/models/products.ts
@@ -2,7 +2,7 @@ import client from "../database";
 
 // product type
 export type Product = {
-  id: number;
+  id?: number;
   name: string;
   price: number;
   category: string;
@@ -19,7 +19,40 @@ export class ProductModel {
       connection.release();
       return result.rows;
     } catch (error) {
-      throw new Error("Failed to get products from database");
+      throw new Error(`Failed to get products from database ${error}`);
+    }
+  }
+
+  // get a product by id
+  async get(id: number): Promise<Product> {
+    try {
+      const connection = await client.connect();
+      const sql = "SELECT * FROM products WHERE id = $1";
+      const result = await connection.query(sql, [id]);
+      connection.release();
+      return result.rows[0];
+    } catch (error) {
+      throw new Error(
+        `Failed to get product with id ${id} from database ${error}`
+      );
+    }
+  }
+
+  // create a new product in the database
+  async create(product: Product): Promise<Product> {
+    try {
+      const connection = await client.connect();
+      const sql =
+        "INSERT INTO products (name, price, category) VALUES ($1, $2, $3) RETURNING *";
+      const result = await connection.query(sql, [
+        product.name,
+        product.price,
+        product.category,
+      ]);
+      connection.release();
+      return result.rows[0];
+    } catch (error) {
+      throw new Error(`Failed to create product in database ${error}`);
     }
   }
 }
--- src/models/tests/productSpec.ts
+++ src/models/tests/productSpec.ts
@@ -2,9 +2,23 @@
 import { ProductModel } from "../products";
 
 describe("ProductModel Tests", () => {
+  // testing index()
   it("should return an array of products", async () => {
     const productModel = new ProductModel();
     const products = await productModel.index();
     expect(products).toBeInstanceOf(Array);
+  });
+  // testing create() and get()
+  it("should return a product", async () => {
+    const productModel = new ProductModel();
+    const product1 = await productModel.create({
+      name: "Test Product",
+      price: 100,
+      category: "Test Category",
+    });
+
+    const product2 = await productModel.get(1);
+
+    expect(product1).toEqual(product2);
   });
 });
